52bdbccaeb6e0dce3645456d3c5c7be26bbb743b
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-02-29 19:23:44 +0530

    Update Main.java

--- RandomizedQueue/src/Main.java
+++ RandomizedQueue/src/Main.java
@@ -6,20 +6,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args)
     {
-        RandomizedQueue<Integer> queue=new RandomizedQueue<Integer>();
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.enqueue(40);
-        queue.enqueue(50);
-        queue.enqueue(60);
-        queue.enqueue(70);
-        System.out.println(queue.sample());
-        Iterator<Integer> itr =queue.iterator();
-        while(itr.hasNext())
-        {
-            System.out.print(itr.next()+" ");
-        }
+       //test the code over here 
 
     }
 }
